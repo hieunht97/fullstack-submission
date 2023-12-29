3f7ef6797b2325206fa939b7356e2e92a60faea6
Hieu Tran <hieunht97@gmail.com>
2023-12-28 22:31:03 -0500

    Part 3

---
+++ part3/phonebookbackend/index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+var morgan = require("morgan");
+
+app.use(express.json());
+
+morgan(":method :url :status :res[content-length] - :response-time ms");
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateID = () => {
+  const id = Math.floor(Math.random() * (10000 - 1 + 1) + 1);
+  return id;
+};
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get("/info", (request, response) => {
+  const number = persons.length;
+  const date = new Date();
+  response.send(
+    `<div><p>Phonebook has info for ${number} people</p><p>${date}</p></div>`
+  );
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  const existingName = persons.find((person) => person.name === body.name);
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number cannot be empty",
+    });
+  } else if (existingName) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    id: generateID(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = 3002;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
